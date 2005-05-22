9b719931595bfdee74c92a9bab86a5db6912274a
Marcin Zukowski <marcin.zukowski@gmail.com>
2005-05-22 23:15:11 +0000

    - added mkdist.sh that generates lepicon-src.tgz - added .cvsignore

---
+++ lepix/lepicon/mkdist.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+tar czvf lepicon-src.tgz \
+	lepicon.c bin2c.c Makefile \
+	common.c common.h \
+	mod_hr0.asx mod_hr0.c mod_hr0.h \
+	mod_hr1.asx mod_hr1.c mod_hr1.h \
+	mod_hr2.asx mod_hr2.c mod_hr2.h \
+
