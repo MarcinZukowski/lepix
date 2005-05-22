1d5905f97f119271d0ca08ff610195af8bffab97
Marcin Zukowski <marcin.zukowski@gmail.com>
2005-05-22 23:23:31 +0000

    - create both binary and source packages with a version number

--- lepix/lepicon/mkdist.sh
+++ lepix/lepicon/mkdist.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-tar czvf lepicon-src.tgz \
+VERSION="0.2"
+
+rm -f lepicon-$VERSION.zip lepicon-src-$VERSION.tgz lepicon
+
+make clean
+make
+strip lepicon.exe
+tar czvf lepicon-$VERSION.tgz \
+	lepicon.exe \
+	
+tar czvf lepicon-src-$VERSION.tgz \
 	lepicon.c bin2c.c Makefile \
 	common.c common.h \
 	mod_hr0.asx mod_hr0.c mod_hr0.h \
 	mod_hr1.asx mod_hr1.c mod_hr1.h \
 	mod_hr2.asx mod_hr2.c mod_hr2.h \
-
+	
+	
